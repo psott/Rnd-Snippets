73f63e6e6277db1b9c4aed7409c8b8514519a10e
psott <info@a-ott.com>
2017-09-29 11:32:21 +0200

    Create Alias-Bullshit.ps1

---
+++ Other/Alias-Bullshit.ps1
@@ -0,0 +1,7 @@
+# how to call specific commandlets when vmware and scvmm module are both loaded /installed
+
+Get-Job                             #Get-SCJob
+Microsoft.PowerShell.Core\Get-Job   #Get-Job
+
+Get-VM                              #Get-SCVirtualMachine
+VMware.VimAutomation.Core\Get-VM    #Get-VM - vmware
