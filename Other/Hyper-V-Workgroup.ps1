4195074599a222f10abe3b5d96f23f9d462cc221
psott <info@a-ott.com>
2018-10-17 11:22:01 +0200

    Update Hyper-V-Workgroup.ps1

--- Other/Hyper-V-Workgroup.ps1
+++ Other/Hyper-V-Workgroup.ps1
@@ -1,2 +1,8 @@
 winrm set winrm/config/client @{TrustedHosts="*"} 
 cmdkey /add:srv1 /user:ADMINISTRATOR /pass:Password
+
+Get-NetConnectionProfile | where{$_.NetworkCategory -eq 'Public'} | foreach{
+    Set-NetConnectionProfile -InterfaceIndex $_.InterfaceIndex -NetworkCategory Private
+}
+Enable-PSRemoting -Force
+Set-Item WSMan:\localhost\Client\TrustedHosts *
