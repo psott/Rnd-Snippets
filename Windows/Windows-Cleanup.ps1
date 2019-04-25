3221b2ad88bbb760a49b4409a286e242f75119d1
psott <info@a-ott.com>
2019-04-25 07:48:48 +0200

    Update Windows-Cleanup.ps1

--- Windows/Windows-Cleanup.ps1
+++ Windows/Windows-Cleanup.ps1
@@ -1,3 +1,8 @@
 #Remove 3D Objects
 Get-Item 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\MyComputer\NameSpace\{0DB7E03F-FC29-4DC6-9020-FF41B59E513A}' | Remove-Item
 Get-Item 'HKLM:\SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Explorer\MyComputer\NameSpace\{0DB7E03F-FC29-4DC6-9020-FF41B59E513A}' | Remove-Item
+
+
+#iCloud Photos
+Get-Item 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\MyComputer\NameSpace\{F0D63F85-37EC-4097-B30D-61B4A8917118}' | Remove-Item
+Get-Item 'HKLM:\SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Explorer\MyComputer\NameSpace\{F0D63F85-37EC-4097-B30D-61B4A8917118}' | Remove-Item
