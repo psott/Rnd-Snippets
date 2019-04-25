03ec341b36268c756b554d6b12d6447433e7b214
psott <info@a-ott.com>
2019-04-25 07:43:55 +0200

    Create Windows-Cleanup.ps1

---
+++ Windows/Windows-Cleanup.ps1
@@ -0,0 +1,3 @@
+#Remove 3D Objects
+Get-Item 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\MyComputer\NameSpace\{0DB7E03F-FC29-4DC6-9020-FF41B59E513A}' | Remove-Item
+Get-Item 'HKLM:\SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Explorer\MyComputer\NameSpace\{0DB7E03F-FC29-4DC6-9020-FF41B59E513A}' | Remove-Item
